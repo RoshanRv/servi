96e5b7a78819a28d889b0227023e25fc9d49a9dd
Roshan Kumar N K <75238302+RoshanRv@users.noreply.github.com>
2023-04-17 10:08:15 +0000

    form updated with react-hook-forms

--- client/components/Form.tsx
+++ client/components/Form.tsx
@@ -1,17 +1,26 @@
-import { View ,Text} from "react-native"
+import { View ,Text, TextInput} from "react-native"
+import {Controller,useForm} from 'react-hook-form'
 
 interface Props{
 
 }
 
+interface FormType{
+    name:string
+    age:number
+}
+
 const Form = ({}:Props)=>{
+
+
+    const {control,handleSubmit,formState:{errors}} = useForm<FormType>()
+
     return (
         <View className="bg-gray-100 dark:bg-gray-900 p-3 rounded-lg shadow-xl shadow-black " >
-            {/* Title */}
-            {/* <Text className="text-pri text-3xl" style={{
-                fontFamily:'RalewayBold'
-            }} ></Text> */}
-            
+            <Controller rules={{
+                required:'true',
+
+            }} control={control} name="name" render={()=><TextInput inputMode="text"  />} />
 
         </View>
     )
