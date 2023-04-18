1b969b2c1eb5b741ce3070a3db9af4de51099331
RoshanRv <nkroshankumar@gmail.com>
2023-04-18 07:27:45 +0530

    backBtn comp created

---
+++ client/components/BackBtn.tsx
@@ -0,0 +1,26 @@
+import { View, Text, TouchableOpacity } from "react-native"
+import React from "react"
+import { Ionicons } from "@expo/vector-icons"
+import { COLORS } from "../utils/constants"
+
+type Props = {
+    handleBack: (no: number) => void
+    no: number
+}
+
+const BackBtn = ({ handleBack, no }: Props) => {
+    return (
+        <TouchableOpacity
+            className="absolute top-3 left-3"
+            onPress={() => handleBack(no)}
+        >
+            <Ionicons
+                name="ios-chevron-back-circle"
+                size={36}
+                color={COLORS.pri}
+            />
+        </TouchableOpacity>
+    )
+}
+
+export default BackBtn
