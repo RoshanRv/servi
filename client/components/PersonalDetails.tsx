f8373d3b2b22b71acc807157dde3a8f32e39bb52
RoshanRv <nkroshankumar@gmail.com>
2023-04-18 07:28:54 +0530

    personal Details updated

--- client/components/PersonalDetails.tsx
+++ client/components/PersonalDetails.tsx
@@ -1,55 +1,85 @@
-import { View, Text, TouchableOpacity } from "react-native"
-import React from "react"
+import { View, Text, TouchableOpacity, TextInput } from "react-native"
+import React, { useEffect } from "react"
 import RoleCard, { Roles } from "./RoleCard"
+import { Controller, useForm } from "react-hook-form"
+import Form from "./Form"
+import BackBtn from "./BackBtn"
 
 type Props = {
     role: "" | Roles
-    setRole: (role: Roles) => void
     handleNext: (np: number) => void
+    handleBack: (np: number) => void
 }
 
-const SelectRole = ({ role, setRole, handleNext }: Props) => {
+interface PersonalDetailsProps {
+    username: string
+    phone: string
+}
+
+const PersonalDetails = ({ role, handleNext, handleBack }: Props) => {
+    const {
+        control,
+        handleSubmit,
+        setError,
+        formState: { errors },
+    } = useForm<PersonalDetailsProps>({})
+
     return (
         <View className="flex flex-col justify-center items-center w-full h-full">
+            <BackBtn handleBack={handleBack} no={1} />
             {/*   Title   */}
             <Text
                 style={{
                     fontFamily: "RalewayBold",
                 }}
-                className="text-3xl text-pri"
+                className="text-3xl text-pri mb-10"
             >
                 Enter Your Details
             </Text>
             {/*   Form   */}
-            <View className="flex flex-row justify-around items-center w-full mt-16">
-                <RoleCard
-                    selectedRole={role}
-                    setRole={setRole}
-                    role="user"
-                    src={require("../assets/user-avatar.webp")}
-                />
-                <RoleCard
-                    selectedRole={role}
-                    setRole={setRole}
-                    role="worker"
-                    src={require("../assets/worker-avatar.webp")}
-                />
-            </View>
+            <Form
+                control={control}
+                errors={errors}
+                inputs={[
+                    {
+                        inputName: "username",
+                        inputType: "text",
+                        icon: "user-alt",
+                        inputOptions: {
+                            required: true,
+                        },
+                    },
+                    {
+                        inputName: "phone",
+                        inputType: "tel",
+                        icon: "phone-alt",
+                        inputOptions: {
+                            required: true,
+                            maxLength: 11,
+                        },
+                    },
+                ]}
+            />
 
             <TouchableOpacity
                 disabled={role ? false : true}
                 className="px-6 py-2 mt-10  bg-white dark:bg-black border-2 border-pri rounded-lg shadow-lg shadow-black  "
-                onPress={() => handleNext(role === "user" ? 2 : 1)}
+                onPress={handleSubmit(
+                    (e) => console.log(e),
+                    (e) => console.log(e)
+                )}
             >
                 <Text
                     style={{
                         fontFamily: "RalewayBold",
                     }}
                     className="capitalize text-lg text-sec"
-                >{role==='user'?'Sign Up':'Next'}</Text>
+                >
+                    {role === "user" ? "Sign Up" : "Next"}
+                </Text>
             </TouchableOpacity>
         </View>
     )
 }
 
-export default SelectRole
+export default PersonalDetails
