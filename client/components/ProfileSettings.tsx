88b0b7a839ba60704002eef8b527c298d19770e5
RoshanRv <nkroshankumar@gmail.com>
2023-04-21 07:35:49 +0530

    profile settings upadated

--- client/components/ProfileSettings.tsx
+++ client/components/ProfileSettings.tsx
@@ -1,8 +1,10 @@
-import { View, Text, Image } from "react-native"
+import { View, Text, Image, TouchableOpacity } from "react-native"
 import React from "react"
 import Layout from "./Layout"
 import useUser from "../store/useUser"
 import NotSignedProfile from "./NotSignedProfile"
+import { FontAwesome5, Ionicons } from "@expo/vector-icons"
+import { SIZES } from "../utils/constants"
 
 type Props = {
     navigation: any
@@ -14,6 +16,7 @@ const ProfileSettings = ({ navigation }: Props) => {
     return (
         <Layout>
             <View className="w-32 h-32 mx-auto bg-pri rounded-full border-2 border-white overflow-hidden ">
+                {/* Image of User/ Worker */}
                 <Image
                     source={
                         user?.role === "worker"
@@ -25,6 +28,7 @@ const ProfileSettings = ({ navigation }: Props) => {
             </View>
             {/*    Not Signed In  (SignIn/Up Button) */}
             {!user && <NotSignedProfile navigation={navigation} />}
+            {/*    Signed In - Username  */}
             {user && (
                 <Text
                     style={{
@@ -35,6 +39,53 @@ const ProfileSettings = ({ navigation }: Props) => {
                     {user.name}
                 </Text>
             )}
+            {/*    Settings   */}
+            <View className="w-[90%] rounded-lg bg-white dark:bg-gray-900 p-3">
+                {/*    User Details  - User/Worker Only */}
+                {user && (
+                    <TouchableOpacity className=" flex flex-row items-center justify-between">
+                        <View className="flex flex-row">
+                            <FontAwesome5
+                                icon="user-alt"
+                                size={SIZES.lg}
+                                color={"gray"}
+                            />
+                            <Text
+                                className="text-gray-800 text-lg pl-4"
+                                style={{
+                                    fontFamily: "RalewayRegular",
+                                }}
+                            >
+                                Edit User Details
+                            </Text>
+                        </View>
+                        <Ionicons
+                            name="ios-chevron-forward"
+                            size={SIZES.lg}
+                            color={"gray"}
+                        />
+                    </TouchableOpacity>
+                )}
+                {/*     Select Language     */}
+                <View className=" flex flex-row items-center justify-between">
+                    <View className="flex flex-row">
+                        <Ionicons
+                            name="language"
+                            size={SIZES.lg}
+                            color={"gray"}
+                        />
+                        <Text
+                            className="text-gray-800 text-lg pl-4"
+                            style={{
+                                fontFamily: "RalewayRegular",
+                            }}
+                        >
+                            Select Language
+                        </Text>
+                    </View>
+                    {/*  Select Input... */}
+                </View>
+            </View>
         </Layout>
     )
 }
